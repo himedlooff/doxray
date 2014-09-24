80f217e2f543ef832843bb9290d61f862eab7fea
Mike Morici <Michael.Morici@cfpb.gov>
2014-09-24 13:03:42 -0400

    Modularizing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,48 +10,95 @@ module.exports = function(grunt) {
 
   grunt.initConfig(config);
 
-  grunt.registerTask('docs', 'A new documentation generator.', function(file) {
+  grunt.registerTask('docs', 'A new documentation generator.', function( file ) {
     
     var fs;
 
-    // Check to see if we have the required file to search for
-    if (arguments.length === 0) {
-      grunt.log.writeln(this.name + ', no args');
-    } else {
-      grunt.log.writeln(this.name + ': ' + file);
+    if ( argsAreValid( arguments, this.name ) ) {
       init();
-      getFile( file );
+      parseFile( file );
     }
 
     function init() {
-      grunt.log.writeln('init()');
+      grunt.verbose.writeln('init(): Setting up vars.');
 
       fs = require('fs-extra');
     }
 
-    function getFile( file ) {
-      grunt.log.writeln('getFile()');
-
+    function parseFile( file ) {
       var data, regex, docs, code;
-      data = fs.readFileSync(file, 'utf-8');
-      regex = {
-        topdoc: /\/\*\s*topdoc[^*]*\*+(?:[^/*][^*]*\*+)*\//g
-      };
-      // Remove anything before the first topdoc comment
-      data = data.slice(data.search(regex.topdoc)).trim();
-      // Save just the topdoc comments
-      docs = data.match(regex.topdoc);
-      // Save everything between the topdoc comments
-      code = data.split(regex.topdoc);
-      // Removes the first item in the array since it will always be empty
-      code.shift();
 
-      grunt.log.writeln('docs.length:', docs.length);
-      grunt.log.writeln('code.length:', code.length);
+      grunt.verbose.writeln( 'parseFile(): Parsing', file );
 
-      grunt.log.writeln('\ndocs\n', docs);
-      grunt.log.writeln('\ncode\n', code);
+      regex = /\/\*\s*topdoc[^*]*\*+(?:[^/*][^*]*\*+)*\//g;
+      data = getData( file, regex );
+      docs = parseOutDocs( data, regex );
+      code = parseOutCode( data, regex );
+
+      if ( parsingIsValid( docs, code ) ) {
+        grunt.log.ok('Parsing was validated.');
+        grunt.verbose.writeln( 'docs:\n', docs );
+        grunt.verbose.writeln( 'code:\n', code );
+      }
+
     }
+
+    function parsingIsValid( docs, code ) {
+      // Check to see if the docs and code array lengths match.
+      // If they don't then something went wrong as they should match
+      // one for one.
+      grunt.verbose.writeln( '\tdocs.length:', docs.length );
+      grunt.verbose.writeln( '\tcode.length:', code.length );
+      if ( docs.length !== code.length ) {
+        grunt.log.error( 'Parsing failed because the parsed docs did not match the parsed code.' );
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    function getData( file, regex ) {
+      var data;
+      // Read the file.
+      data = fs.readFileSync( file, 'utf-8' );
+      // Trim everything before the first regex because it's not associated with
+      // any comment.
+      data = data.slice( data.search(regex) );
+      return data;
+    }
+
+    function parseOutDocs( data, regex ) {
+      var docs;
+      // "docs" are anything that matches the regex.
+      docs = data.match( regex );
+      return docs;
+    }
+
+    function parseOutCode( data, regex ) {
+      var code;
+      // The "code" is everything betwixt the regex.
+      code = data.split( regex );
+      // Removes the first item in the array since it will always be empty.
+      code.shift();
+      // Clean each item in the array.
+      code.forEach( trimArrayElement );
+      return code;
+    }
+
+    function trimArrayElement( element, index, array ) {
+      array[index] = array[index].trim();
+    }
+
+    function argsAreValid( args, name ) {
+      // Check to see if we have the required file argument.
+      if ( args.length === 0 ) {
+        grunt.log.error( 'Please provide a file as the first argument.' );
+        return false;
+      } else {
+        return true;
+      }
+    }
+
   });
 
 };
