35c08de8ac824ec6f200ce0622f4de0221e0a5c9
Mike Morici <Michael.Morici@cfpb.gov>
2014-10-09 20:27:04 -0400

    Fixes syntax error.

--- test/index.js
+++ test/index.js
@@ -139,10 +139,10 @@ describe('#ifPropertyMatches', function() {
     var returnValue1, returnValue2;
     commentDocs.ifPropertyMatches( { foo: 'bar' }, 'foo', function( value ) {
       returnValue1 = value;
-    }),
+    });
     commentDocs.ifPropertyMatches( { foo: 'bar' }, 'bar', function( value ) {
       returnValue2 = value;
-    }),
+    });
     assert.equal( returnValue1, 'bar' );
     assert.equal( returnValue2, undefined );
   });
