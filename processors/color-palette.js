44c5da19019bc87e70d4aedde02e271860adb004
himedlooff <mike@bounceexchange.com>
2015-05-02 20:18:23 -0400

    Cleans up the color palette processor code a little.

--- processors/color-palette.js
+++ processors/color-palette.js
@@ -1,31 +1,33 @@
 module.exports = function( parsed ) {
   function makeColorPalette( docCodePair, docObject ) {
-    var results = docCodePair.code.filter(function( oneCodeFile ) {
-      // Only return a code file if it matches the file type specified in the
-      // colorPalette property.
-      return oneCodeFile.type === ('.' + docObject.colorPalette);
-    });
-    var palette = [];
-    if ( results.length ) {
-      code = results[ 0 ].code
-        .replace(/(\/\*([\s\S]*?)\*\/)|(\/\/(.*)$)/gm, '')
-        .replace(/ +?/gm, '')
-        .replace(/(\r\n|\n|\r)/gm, '')
-        .split(';');
-      for ( k = 0; k < code.length; k++ ) {
-        var ruleAsArray = code[ k ].split(':');
-        if ( ruleAsArray.length === 2 ) {
-          var key = ruleAsArray[ 0 ];
-          var val = ruleAsArray[ 1 ];
-          if ( val.charAt( 0 ) !== '$' ) {
-            palette.push({
-              variable: key,
-              value: val
-            });
+    if ( docObject.colorPalette ) {
+      var results = docCodePair.code.filter(function( oneCodeFile ) {
+        // Only return a code file if it matches the file type specified in the
+        // colorPalette property.
+        return oneCodeFile.type === ('.' + docObject.colorPalette);
+      });
+      var palette = [];
+      if ( results.length ) {
+        code = results[ 0 ].code
+          .replace(/(\/\*([\s\S]*?)\*\/)|(\/\/(.*)$)/gm, '')
+          .replace(/ +?/gm, '')
+          .replace(/(\r\n|\n|\r)/gm, '')
+          .split(';');
+        for ( k = 0; k < code.length; k++ ) {
+          var ruleAsArray = code[ k ].split(':');
+          if ( ruleAsArray.length === 2 ) {
+            var key = ruleAsArray[ 0 ];
+            var val = ruleAsArray[ 1 ];
+            if ( val.charAt( 0 ) !== '$' ) {
+              palette.push({
+                variable: key,
+                value: val
+              });
+            }
           }
         }
+        docObject.colorPalette = palette;
       }
-      docObject.colorPalette = palette;
     }
   }
   parsed.files.forEach(function( file, fileIndex ){
@@ -34,15 +36,10 @@ module.exports = function( parsed ) {
       // If it's an array of docs, loop through it.
       if ( Array.isArray( docCodePair.docs ) ) {
         docCodePair.docs.forEach(function( doc, docIndex ){
-          if ( docCodePair.docs[ docIndex ].colorPalette ) {
-            makeColorPalette( docCodePair, doc );
-          }
+          makeColorPalette( docCodePair, doc );
         });
       } else {
-        if ( docCodePair.docs.colorPalette ) {
-          var palette = [];
-          makeColorPalette( docCodePair, docCodePair.docs );
-        }
+        makeColorPalette( docCodePair, docCodePair.docs );
       }
     });
   });
